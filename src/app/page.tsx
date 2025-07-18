b91eedf8c9e7836bd23e10bd2a5d6b74f2d7d7a5
luqueee <luqueee_@outlook.es>
2025-07-18 15:08:48 +0200

    feat: Add Kena logo to the homepage with link to the project

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -4,6 +4,8 @@ import { TechStack } from "@/components/blocks/TechStack";
 import { Hero } from "@/components/hero/Hero";
 import { Contests } from "@/components/blocks/Contests";
 import { Studies } from "@/components/blocks/Studies";
+import Image from "next/image";
+import Link from "next/link";
 
 export default function Home() {
   return (
@@ -15,6 +17,19 @@ export default function Home() {
       <div className="mt-20 max-w-default w-full scroll-mt-32" id="contests">
         <Contests />
       </div>
+      <div className="mt-20 max-w-default w-full scroll-mt-32 flex justify-center items-center flex-col gap-8">
+        <h3 className="text-3xl font-[700]">Currently working on</h3>
+        <Link href={"https://kenabot.xyz"} target="_blank">
+          <Image
+            src={"/images/kenaLogo.png"}
+            alt="kena logo"
+            width={80}
+            height={80}
+            className="rounded-lg shadow-lg"
+          />
+        </Link>
+      </div>
+
       <div className="mt-20 max-w-default w-full" id="tech-stack">
         <TechStack />
       </div>
